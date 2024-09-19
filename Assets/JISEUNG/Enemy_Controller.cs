48ff5c322d96d91356fe4963c1bf7577b99ac636
UmJi <147022364+userUmji@users.noreply.github.com>
2024-09-19 22:20:43 +0900

    240919
    1. 충돌시 플레이어 HP감소

--- Assets/JISEUNG/Enemy_Controller.cs
+++ Assets/JISEUNG/Enemy_Controller.cs
@@ -2,32 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Controller : MonoBehaviour
+public class Enemy_Controller : Status
 {
+    public  GameObject[]  player_List; 
+
     public GameObject player;
-    public float speed;
 
     Vector2 dir;
-    private void Start()
+
+    public override void Start()
     {
+        base.Start();
         player = GameObject.Find("Player");
+
+        player_List = GameObject.FindGameObjectsWithTag("Player");
+        int num = Random.Range(0, player_List.Length);
+        player = player_List[num]; 
+
+       
     }
     // Update is called once per frame
     private void FixedUpdate()
+    {
+
+        dir = player.transform.position - transform.position;
+        dir = dir.normalized;
+
+        transform.Translate(dir * curSpeed * Time.deltaTime);
+    }
+
+/*    private void Update()
     {
         //transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed);
 
         dir = player.transform.position - transform.position;
         dir=dir.normalized;
 
-        transform.Translate(dir * speed);
-    }
+        transform.Translate(dir * curSpeed * Time.deltaTime);
+    }*/
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            print("플레이어 충돌");
+            collision.GetComponent<Unit>().GetDamege(this); 
         }
     }
 }
